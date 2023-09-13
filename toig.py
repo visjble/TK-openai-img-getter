3a127925da536f8fd99dcb6fd2c26739a9472836
visjble <leodavinci111@gmail.com>
2023-09-12 17:25:04 -0700

    Update toig.py

--- toig.py
+++ toig.py
@@ -1,25 +1,44 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import filedialog, messagebox
 import openai
 from PIL import Image, ImageTk
 from io import BytesIO
-import os, requests
+import os
+import requests
 
+class OpenAIApp:
     def __init__(self, root):
         self.root = root
         self.root.title("OpenAI Image Generator")
 
-        # OpenAI API Key Entry
-        self.api_label = tk.Label(root, text="OpenAI API Key:")
-        self.api_label.pack(pady=10)
-        self.api_entry = tk.Entry(root, width=50)
-        self.api_entry.pack(pady=10)
+        self.root.geometry("512x800")
+        self.root.configure(bg="#2E2E2E")  # Dark gray background
 
-        # Description Entry
-        self.desc_label = tk.Label(root, text="Enter Description:")
-        self.desc_label.pack(pady=10)
-        self.desc_entry = tk.Entry(root, width=50)
-        self.desc_entry.pack(pady=10)
+        # Frame for API Key with dark gray background
+        api_frame = tk.Frame(root, bg="#2E2E2E")
+        api_frame.pack(pady=10, fill=tk.X)
+
+        # OpenAI API Key Entry with light gray text
+        self.api_label = tk.Label(api_frame, text="OpenAI API Key:", width=15, bg="#2E2E2E", fg="#D8D8D8")
+        self.api_label.pack(side=tk.LEFT, padx=5)
+        self.api_entry = tk.Entry(api_frame, width=50, show="*", bg="#3C3C3C", fg="#D8D8D8")
+        self.api_entry.pack(side=tk.RIGHT, padx=5)
+
+        # Frame for Description with dark gray background
+        desc_frame = tk.Frame(root, bg="#2E2E2E")
+        desc_frame.pack(pady=10, fill=tk.X)
+
+        # Description Entry with light gray text
+        self.desc_label = tk.Label(desc_frame, text="Prompt:", width=15, bg="#2E2E2E", fg="#D8D8D8")
+        self.desc_label.pack(side=tk.LEFT, padx=5)
+        self.desc_entry = tk.Entry(desc_frame, width=50, bg="#3C3C3C", fg="#D8D8D8")
+        self.desc_entry.pack(side=tk.RIGHT, padx=5)
+
+        
+
+        # Bind the Enter key to the generate_image function
+        self.api_entry.bind('<Return>', self.generate_image)
+
 
         # Frame for buttons
         self.button_frame = tk.Frame(root)
@@ -28,13 +47,13 @@ import os, requests
         # Frame for Generate Image Button
         self.generate_frame = tk.Frame(self.button_frame)
         self.generate_frame.pack(side=tk.LEFT, padx=5)
-        self.generate_btn = tk.Button(self.generate_frame, text="Generate Image", command=self.generate_image)
-        self.generate_btn.pack()
+        self.generate_btn = tk.Button(self.generate_frame, text="Generate Image", command=self.generate_image, bg="#4B4B4B", fg="#D8D8D8", activebackground="#5A5A5A", activeforeground="#D8D8D8")
+        self.generate_btn.pack(pady=5)
 
         # Frame for Save Image Button
         self.save_frame = tk.Frame(self.button_frame)
-        self.save_frame.pack(side=tk.LEFT, padx=5)
-        self.save_btn = tk.Button(self.save_frame, text="Save Image", command=self.save_image, state=tk.DISABLED)  # Initially disabled
+        self.save_frame.pack(side=tk.LEFT, fill=tk.X, padx=5)
+        self.save_btn = tk.Button(self.save_frame, text="Save Image", command=self.save_image, state=tk.DISABLED, bg="#4B4B4B", fg="#D8D8D8", activebackground="#5A5A5A", activeforeground="#D8D8D8")  # Initially disabled
         self.save_btn.pack()
 
         # Image Label
@@ -84,5 +103,6 @@ import os, requests
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("512x800")
     app = OpenAIApp(root)
     root.mainloop()
